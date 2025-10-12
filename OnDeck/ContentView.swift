bea2f5a457fa55fe8b16cd81aee39d814f9cae38
Danny Eskandar <152567869+danielllesk@users.noreply.github.com>
2025-10-12 01:37:50 -0400

    Initial Commit

---
+++ OnDeck/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OnDeck
+//
+//  Created by Danny Eskandar on 2025-10-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnDeck/OnDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnDeckApp.swift
+//  OnDeck
+//
+//  Created by Danny Eskandar on 2025-10-12.
+//
+
+import SwiftUI
+
+@main
+struct OnDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
